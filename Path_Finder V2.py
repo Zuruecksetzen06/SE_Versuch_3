8f4ee7bfb12c8e0a9e1f458066d51f177a1779cb
Zuruecksetzen06 <lspieper15@gmail.com>
2026-01-10 18:36:34 +0100

    Changed Version of Pathfinder

--- Path_Finder V2.py
+++ Path_Finder V2.py
@@ -1,20 +1,10 @@
-
-
 import Test_Data
 import Start_End
-import time
-import Visualizer
-
-test_map = Test_Data.Normalised
-pos, target_pos = Start_End.findStartEnd(test_map)
-
-direction = 'W'
-
-forbidden_list = ['S', 'B', 'W']
 
 # N, E, S, W
 # r, f, l, b
 
+forbidden_list = ['S', 'B', 'W']
 rotation_list = ['r', 'f', 'l', 'b']
 
 def get_next_direction(direction, rotation):
@@ -63,32 +53,42 @@ def get_field(check_direction, pos):
     if check_direction == 'W':
         return [pos[0], pos[1]-1]
     
-def get_next_move(go_back_mode, rotation_index = 0):
+def get_next_move(pos, direction, lab_map, go_back_mode = False, rotation_index = 0):
     rotation = rotation_list[rotation_index]
     check_direction = get_next_direction(direction, rotation)
     check_field = get_field(check_direction, pos)
     if go_back_mode:
-        if test_map[check_field[0]][check_field[1]] != 'W':
+        if lab_map[check_field[0]][check_field[1]] != 'W':
             return check_field, check_direction
-        return get_next_move(True, rotation_index+1)
+        return get_next_move(pos, direction, lab_map, True, rotation_index+1)
         
     else:         
-        if not test_map[check_field[0]][check_field[1]] in ['S', 'W', 'B']:
+        if not lab_map[check_field[0]][check_field[1]] in ['S', 'W', 'B']:
             return check_field, check_direction
         if rotation_index == 3:
-            return get_next_move(True, 0)
+            return get_next_move(pos, direction, lab_map, True, 0)
         else:
-            return get_next_move(False, rotation_index+1)
+            return get_next_move(pos, direction, lab_map, False, rotation_index+1)
         
-while pos != target_pos:
-    pos, direction = get_next_move(False, 0)
+def move(pos, direction, lab_map):
 
-    test_map = [['B' if element == 'A' else element for element in reihe] for reihe in test_map]
+    next_pos, next_direction = get_next_move(pos, direction, lab_map)
+    lab_map = [['B' if element == 'A' else element for element in reihe] for reihe in lab_map]
 
-    if test_map[pos[0]][pos[1]] != 'S':
-        test_map[pos[0]][pos[1]] = 'A'
-    
-    Test_Data.printData(test_map)
-    #time.sleep(1)
-    #Visualizer.run_labyrinth_view(test_map)
+    if lab_map[next_pos[0]][next_pos[1]] != 'S':
+        lab_map[next_pos[0]][next_pos[1]] = 'A'
+    return next_pos, next_direction, lab_map
+
+def main():
+    test_map = Test_Data.Normalised
+    pos, target_pos = Start_End.findStartEnd(test_map)
+    direction = 'W'
+
+    while target_pos != pos:
+        pos, direction, test_map = move(pos, direction, test_map)
+        Test_Data.printData(test_map)
+
+
+if __name__ == '__main__':
+    main()
 
